85af2da891b3c097826cf5c784e19551b5ca334e
Cameron Priestley <cameronpriestley@gmail.com>
2016-05-16 00:18:48 -0400

    missed connections

--- sketch.js
+++ sketch.js
@@ -1,20 +1,41 @@
 
-var check = 'two files';
-var entry;
+var request = require("request"),
+    cheerio =require("cheerio"),
+    url="https://newyork.craigslist.org/search/mis";
+
+    request(url, function(error, response, body){
+        if(!error){
+            var $ = cheerio.load(body),
+            // titles= $('div[class=rows]').html()
+            titles= $('span[id=titletextonly]').html()
+
+                var config = require('./config.js');
+                var T = new Twit(config);
+
+                function poster (){
+                    T.post('statuses/update', {status: titles},
+                    function (err, data, response){
+                    console.log(data);
+                    console.log(err);
+                    console.log(response);
 
 
-//
-var url = 'https://newyork.craigslist.org/search/mis?format=rss&sort=date&query=w4m';
-feednami.load(url,function(result){
-        if(result.error){
-                console.log(result.error)
-        }
-        else{
-            var entries = result.feed.entries
-                for(var i = 0; i < entries.length; i++){
-                    entry = entries[i];
-                    check.push(entry);
-                    console.log(entry.title)
+                });
                 }
-            }
-})
+
+                    setInterval(poster, 10*60*1000);
+
+            // var T = new Twit(config);
+
+            // var update = 'statuses/update';
+            // var tweet = {status:'hello world!'};
+
+                        console.log(titles);
+                    }else{
+                        console.log("We’ve encountered an error: " + error);
+                    }
+
+
+    });
+
+var Twit = require('twit');
